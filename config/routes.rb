117d9da149758c21067b6b9899eec5f0cc29cc46
Alex Dunn <dunn.alex@gmail.com>
2017-08-22 10:25:07 -0700

    add role management

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'catalog/:id' => 'catalog#show', as: 'catalog_id'
 
   devise_for :users
+  mount Hydra::RoleManagement::Engine => '/'
+
   mount Qa::Engine => '/authorities'
   mount Hyrax::Engine, at: '/'
   resources :welcome, only: 'index'
